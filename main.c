9213c70a57e95c875402ad1f55c7b51df89b35fb
2215778 <dlcoals0223@naver.com>
2022-10-04 18:02:39 +0900

    실습6

--- main.c
+++ main.c
@@ -4,23 +4,26 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) { 
-    int x, y;
-    char op;
-    int result;
+    int answer=38;
+    int x;
+    int trial=0;
     
-    printf("enter the calculation: ");
-    scanf("%d %c %d",&x,&op,&y);
     
-	if(op=='+')
-	    result=x+y;
-	else if(op=='-')
-	    result=x-y;
-	else if(op=='*')
-	    result=x*y;
-	else
-	    result=x/y;
-		 
-	printf("= %i\n",result);
+    do
+    {
+    	printf("Guess a number: "); //입력해라 문구 출력
+		scanf("%d",&x); //입력을 받음
+		trial ++;
+		
+		if(x>answer) //입력숫자가 정답보다 큰지 작은지 출력
+		    printf("high!\n");
+		
+		if(x<answer)
+		    printf("low!\n");
+	}
+	while(x!=answer); //정답과 같은지 여부 판단 
+	
+	printf("Congratulation! trials: %i\n",trial); //시도 횟수 출력 
 	
 	
 	return 0;
