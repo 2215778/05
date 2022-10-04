8e151795e6f5c559b70a522a62a532e295b0cd2f
2215778 <dlcoals0223@naver.com>
2022-10-04 16:30:52 +0900

    실습2

--- main.c
+++ main.c
@@ -10,11 +10,9 @@ int main(int argc, char *argv[]) {
     scanf("%i", &x);
     
     if(x>0)
-        printf("양수입니다.\n");
-    else if(x<0)
-	    printf("음수입니다.\n");
-	else
-	    printf("0입니다.\n");
+        printf("절대값은 %i 입니다.\n", x);
+    else
+	    printf("절대값은 %i 입니다.\n", -x);
 	    
 		 
 	return 0;
