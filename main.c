3d9dbd7850f0500b10c22cb735cc363d08ca277c
2215778 <dlcoals0223@naver.com>
2022-10-04 16:40:53 +0900

    실습2(정석)

--- main.c
+++ main.c
@@ -7,13 +7,13 @@ int main(int argc, char *argv[]) {
     int x;
     
     printf("정수 하나를 입력하시오: ");
-    scanf("%i", &x);
+    scanf("%i",&x);
+	
+	if(x<0)
+	     x=-1*x;
+		 
+    printf("절대값은 %i 입니다.",x); 
     
-    if(x>0)
-        printf("절대값은 %i 입니다.\n", x);
-    else
-	    printf("절대값은 %i 입니다.\n", -x);
-	    
 		 
 	return 0;
 }
